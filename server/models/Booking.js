954e1a55e97036bef86f90fe765b31f583bcff62
sadiqonWSL <sadiqonemail@gmail.com>
2023-10-24 03:32:36 +0530

    server - data model/schema added

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,7 +1,48 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-  // 
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  driver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Driver',
+  },
+  origin: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point',
+    },
+    coordinates: {
+      type: [Number], // Array to store [longitude, latitude]
+    },
+  },
+  destination: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point',
+    },
+    coordinates: {
+      type: [Number], // Array to store [longitude, latitude]
+    },
+  },
+  pickupTime: {
+    type: Date,
+    required: true,
+  },
+  price: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Confirmed', 'Completed', 'Rejected'],
+    // enum: ['Pending', 'Confirmed', 'Completed', 'Cancelled', 'On Route', 'In Progress', 'Delayed', 'Rejected'],
+    default: 'Pending',
+  }
 });
 
 module.exports = mongoose.model('Booking', bookingSchema);
--- server/models/Driver.js
+++ server/models/Driver.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
 const driverSchema = new mongoose.Schema({
-  // 
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Driver', driverSchema);
