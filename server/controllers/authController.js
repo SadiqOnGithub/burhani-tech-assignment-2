24caa4fd03067f218e5e010e7d8d4a41850e0e46
sadiqonWSL <sadiqonemail@gmail.com>
2023-10-24 02:50:34 +0530

    server - comments added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,15 +1,45 @@
 
 
 const login = async (req, res) => {
-  //
+  // get username and p/w from body
+
+  // check username and p/w are there
+
+  // get user from db
+
+  // return 401 Unauthorized if user not found
+
+  // check the password match with the encrypted password in the db
+
+  // return 401 Unauthorized if p/w doesn't match
+
+  // create access and refresh token
+
+  // set refresh token to http-only cookie with other security setting
+
+  // send access token as json
 };
 
 const refresh = (req, res) => {
-  //
+  // get cookies from req
+
+  // check (!cookies?.jwt); if not sent 401 Unauthorized
+
+  // verify refresh token
+  // -- if error send 403 Forbidden
+  // -- get the user from db using decoded username
+  // -- return 401 Unauthorized if user not found
+  // -- create access token
+  // -- send access token as json
+  
 };
 
 const logout = (req, res) => {
-  //
+  // get cookies from req
+
+  // if (!cookies?.jwt) and sent 204
+
+  // clear cookies with same security setting
 };
 
 module.exports = {
--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -1,6 +1,15 @@
 
 const verifyJWT = (req, res, next) => {
-  // JWT verify logic
+  // get authHeader from the req.headers (look for both a and A)
+
+  // if header doesn't start with Bearer then return 401 Unauthorised
+
+  // split and get the token
+
+  // verify the token
+  // -- error return 403 Forbidden
+  // -- setting necessary context in req form decoded token
+  next()
 };
 
 module.exports = verifyJWT; 
