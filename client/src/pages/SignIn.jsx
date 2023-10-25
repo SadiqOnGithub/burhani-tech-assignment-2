90fa4037f3299a1a4b72942ad06d05046850ff1b
sadiqonWSL <sadiqonemail@gmail.com>
2023-10-26 03:28:33 +0530

    SignIn logic added with proper error handling

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,19 +1,55 @@
-import { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import {
-  Container, Typography,
-  TextField, Button,
-  Link, Box,
-} from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Container, Typography, TextField, Button, Link, Box } from '@mui/material';
 
+import axios from '../api/axios';
+import { validationSchema } from '../utils/utils';
 
 function SignIn() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: async (values, { setFieldError }) => {
+      console.log(values);
+      try {
+        const response = await axios.post('/auth', values, {
+          headers: { 'Content-Type': 'application/json' },
+          withCredentials: true,
+        });
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-  };
+        const accessToken = response.data.accessToken;
+
+        // Handle successful login, e.g., store the access token and navigate to the next page
+        console.log('Login successful. Access Token:', accessToken);
+
+      } catch (error) {
+        if (error.response) {
+          if (error.response.status === 400) {
+            // missing username or password
+            setFieldError("username", error.response.data.message);
+            setFieldError("password", error.response.data.message);
+          } else if (error.response.status === 401) {
+            if (error.response.data.message.includes('username')) {
+              setFieldError("username", error.response.data.message);
+            } else {
+              setFieldError("password", "Wrong pssword");
+            }
+            console.log(error.response)
+          } else {
+            // Other server error
+            console.error('Server error:', error.response);
+          }
+        } else {
+          // Network or other client-side errors
+          console.error('Client-side error:', error.message);
+        }
+      }
+    }
+  });
 
   return (
     <>
@@ -35,7 +71,7 @@ function SignIn() {
           </Typography>
           <Box
             component="form"
-            onSubmit={handleLogin}
+            onSubmit={formik.handleSubmit}
             noValidate
             sx={{
               width: '100%',
@@ -43,30 +79,32 @@ function SignIn() {
             }}
           >
             <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username/Email"
-              name="username"
-              autoComplete="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
               autoFocus
+              required
+              id="username"
+              label="Username"
+              autoComplete="username"
+              fullWidth
+              margin="normal"
               sx={{ marginBottom: 2 }}
+
+              {...formik.getFieldProps('username')}  // this line is handling value, name, onChange
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username ? formik.errors.username : ''}
             />
             <TextField
-              margin="normal"
               required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
               id="password"
+              type="password"
+              label="Password"
               autoComplete="current-password"
+              fullWidth
+              margin="normal"
               sx={{ marginBottom: 2 }}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+
+              {...formik.getFieldProps('password')}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password ? formik.errors.password : ''}
             />
             <Button
               type="submit"
@@ -74,10 +112,12 @@ function SignIn() {
               variant="contained"
               color="primary"
               sx={{ marginTop: 2 }}
-              disabled={username === "" || password === ""}
+              disabled={formik.isSubmitting || !formik.isValid}
             >
               Sign In
             </Button>
+
+            {/* extra links */}
             <Box sx={{ mt: 2 }}>
               <Link href="#" variant="body2">
                 Forgot password?
